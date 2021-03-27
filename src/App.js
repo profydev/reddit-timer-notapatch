5f2158c6ec5018d3423d5634cd567ec118da4250
Richard <richard.wigley@gmail.com>
2021-03-27 14:18:59 +0000

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Normalize } from 'styled-normalize';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
     <div>
       <Normalize />
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Montserrat;
+    background-color: #ccc;
+  }
+`;
+
+export default GlobalStyle;
